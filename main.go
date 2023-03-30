d4e2b35c39b583476a925d3acca2ed55727dd95b
guoweizhang <285009003@qq.com>
2023-03-30 12:15:35 +0800

    main函数

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import "net"
-
 func main() {
-	// 定义广播地址
-	broadcastAddr := net.IPv4(255, 255, 255, 255)
 
-	// 定义本地地址
-	localAddr := net.UDPAddr{
-		IP:   net.IPv4zero,
-		Port: 12345,
-	}
-
-	// 创建本地UDP连接
-	conn, err := net.DialUDP("udp", &localAddr, &net.UDPAddr{
-		IP:   broadcastAddr,
-		Port: 12345,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	// 发送数据到广播地址
-	_, err = conn.Write([]byte("Hello, world!"))
-	if err != nil {
-		panic(err)
-	}
-
-	// 关闭连接
-	err = conn.Close()
-	if err != nil {
-		panic(err)
-	}
 }
